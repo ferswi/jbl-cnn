8fd8875a59db3e619c354cfd67c393d46374c0aa
Swiffeerr <sophie.bernard-doucet@umontreal.ca>
2026-06-15 12:04:24 -0400

    Full pre-processing script, time limit is constraining ?

--- chrombpnet_endo_parietal.sh
+++ chrombpnet_endo_parietal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=0-2:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --account=def-jbl
 #SBATCH --mem=64G
 #SBATCH --ntasks=1
@@ -9,6 +9,9 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=sophie.bernard-doucet@umontreal.ca
+
+set -e
+set -o pipefail
 
 module --force purge
 module load StdEnv/2023
@@ -27,7 +30,6 @@ BASE_SC2=/lustre07/scratch/sbernarr/sc2types_cbpn
 BASE_SUBSET=/lustre07/scratch/sbernarr/subset_bam/output
 REF=$BASE_SC2/ref
 
-#todo : one time troublesome downloads:#-------------------------------------------------------------------------
 if [ ! -f $REF/hg38.standard.chrom.sizes ]; then
     wget -q https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes \
         -O $REF/hg38.chrom.sizes
@@ -38,95 +40,155 @@ if [ ! -f $REF/hg38-blacklist.v2.bed.gz ]; then
     wget -q https://github.com/Boyle-Lab/Blacklist/raw/master/lists/hg38-blacklist.v2.bed.gz \
         -O $REF/hg38-blacklist.v2.bed.gz
 fi
-#-------------------------------------------------------------------------
 
-# pre-processing : merge - sort - index
-samtools merge -f $BASE_SC2/parietal_merged_unsorted.bam \
-    $BASE_SUBSET/EBD21_crispri_1a_ex_endo_parietal.bam \
-    $BASE_SUBSET/EBD21_crispri_1b_ex_endo_parietal.bam \
-    $BASE_SUBSET/EBD21_crispri_2a_ex_endo_parietal.bam \
-    $BASE_SUBSET/EBD21_crispri_2b_ex_endo_parietal.bam
+# 1st part of process, merge all the subdivded bam files together.
+if [ ! -f $BASE_SC2/bam/parietal_merged.bam ]; then
+    echo "$(date) -- merging and sorting BAMs"
+    samtools merge -@ 8 - \
+        $BASE_SUBSET/EBD21_crispri_1a_ex_endo_parietal.bam \
+        $BASE_SUBSET/EBD21_crispri_1b_ex_endo_parietal.bam \
+        $BASE_SUBSET/EBD21_crispri_2a_ex_endo_parietal.bam \
+        $BASE_SUBSET/EBD21_crispri_2b_ex_endo_parietal.bam \
+        | samtools sort -@ 8 \
+            -T $BASE_SC2/bam/parietal_sort_tmp \
+            -o $BASE_SC2/bam/parietal_merged.bam
+    samtools index $BASE_SC2/bam/parietal_merged.bam
+else
+    echo "$(date) -- parietal_merged.bam already exists, skipping"
+fi
 
-samtools sort -@4 $BASE_SC2/parietal_merged_unsorted.bam \
-    -o $BASE_SC2/parietal_merged.bam
-samtools index $BASE_SC2/bam/parietal_merged.bam
+# 2nd part of process, we're filtering hihi
+if [ ! -f $BASE_SC2/bam/parietal_filtered.bam ]; then
+  samtools view -@ 8 -F 1804 -f 2 -q 30 \
+      $BASE_SC2/bam/parietal_merged.bam \
+      -o $BASE_SC2/bam/parietal_filtered.bam
+  samtools index $BASE_SC2/bam/parietal_filtered.bam
+else
+    echo "$(date) -- parietal_filtered.bam already exists, skipping filtering"
+fi
 
-samtools view -@ 4 -F 1804 -f 2 -q 30 \
-    $BASE_SC2/parietal_merged.bam \
-    -o $BASE_SC2/parietal_filtered.bam
-samtools index $BASE_SC2/bam/parietal_filtered.bam #todo filter again ?
+#Step 3 of pre-pro, removing duplicates
+if [ ! -f $BASE_SC2/bam/parietal_nodup.bam ]; then
+    echo "$(date) -- removing duplicates"
+    java -Xmx32g -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+        I=$BASE_SC2/bam/parietal_filtered.bam \
+        O=$BASE_SC2/bam/parietal_nodup.bam \
+        M=$BASE_SC2/bam/parietal_dup_metrics.txt \
+        REMOVE_DUPLICATES=true
+    samtools index $BASE_SC2/bam/parietal_nodup.bam
+else
+    echo "$(date) -- parietal_nodup.bam already exists, skipping dedup"
+fi
 
-#todo figure this segment out ?
- java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-    I=$BASE_SC2/bam/parietal_filtered.bam \
-    O=$BASE_SC2/bam/parietal_nodup.bam \
-    M=$BASE_SC2/bam/parietal_dup_metrics.txt \
-    REMOVE_DUPLICATES=true
-#picard MarkDuplicates \
-#    I=$BASE_SC2/parietal_filtered.bam \
-#    O=$BASE_SC2/parietal_nodup.bam \
-#    M=$BASE_SC2/parietal_dup_metrics.txt \
-#    REMOVE_DUPLICATES=true
-#-------------------------------------------------------------------------end of segment to figure out
+# 4e étape processing : shift reads bc of the tn5 correction ? +4bp +strand, -5bp -strand.
+if [ ! -f $BASE_SC2/bam/parietal_shifted_sorted.bam ]; then
+    echo "$(date) -- shifting reads"
+    alignmentSieve \
+        --numberOfProcessors 8 \
+        --ATACshift \
+        --bam $BASE_SC2/bam/parietal_nodup.bam \
+        -o $BASE_SC2/bam/parietal_shifted.bam
+    samtools sort -@ 8 \
+        -T $BASE_SC2/bam/parietal_shift_tmp \
+        $BASE_SC2/bam/parietal_shifted.bam \
+        -o $BASE_SC2/bam/parietal_shifted_sorted.bam
+    samtools index $BASE_SC2/bam/parietal_shifted_sorted.bam
+    rm $BASE_SC2/bam/parietal_shifted.bam
+else
+    echo "$(date) -- parietal_shifted_sorted.bam already exists, skipping shift"
+fi
 
-#shift reads bc of the tn5 correction ? +4bp +strand, -5bp -strand.
-alignmentSieve \
-    --numberOfProcessors 4 \
-    --ATACshift \
-    --bam $BASE_SC2/parietal_nodup.bam \
-    -o $BASE_SC2/parietal_shifted.bam
+#5th step, calling the peaks
+if [ ! -f $BASE_SC2/peaks/parietal/parietal_peaks.narrowPeak ]; then
+    echo "$(date) -- calling peaks"
+    macs2 callpeak \
+        -t $BASE_SC2/bam/parietal_shifted_sorted.bam \
+        -f BAMPE \
+        -n parietal \
+        --outdir $BASE_SC2/peaks/parietal/ \
+        -g hs \
+        --nomodel \
+        --shift -75 \
+        --extsize 150 \
+        --keep-dup all \
+        -B \
+        --SPMR \
+        --call-summits
+else
+    echo "$(date) -- peaks exist, skipping"
+fi
 
-
-samtools sort -@4 $BASE_SC2/parietal_shifted.bam \
-    -o $BASE_SC2/parietal_shifted_sorted.bam
-samtools index $BASE_SC2/parietal_shifted_sorted.bam
-
-#calling peaks
-macs2 callpeak \
-    -t $BASE_SC2/parietal_shifted_sorted.bam \
-    -f BAMPE \
-    -n parietal \
-    --outdir $BASE_SC2/peaks/parietal/ \
-    -g hs \
-    --nomodel \
-    --shift -75 \
-    --extsize 150 \
-    --keep-dup all \
-    -B \
-    --SPMR \
-    --call-summits
 
 #wget https://github.com/Boyle-Lab/Blacklist/raw/master/lists/hg38-blacklist.v2.bed.gz
-bedtools intersect \
-    -v \
-    -a $BASE_SC2/peaks/parietal/parietal_peaks.narrowPeak \
-    -b $REF/hg38-blacklist.v2.bed.gz \
-    > $BASE_SC2/peaks/parietal/parietal_peaks_filtered.narrowPeak
+if [ ! -f $BASE_SC2/peaks/parietal/parietal_peaks_blacklisted.narrowPeak ]; then
+    echo "$(date) -- filtering blacklist from narrowPeak"
+    bedtools intersect \
+        -v \
+        -a $BASE_SC2/peaks/parietal/parietal_peaks.narrowPeak \
+        -b $REF/hg38-blacklist.v2.bed.gz \
+        > $BASE_SC2/peaks/parietal/parietal_peaks_blacklisted.narrowPeak
+else
+    echo "$(date) -- blacklisted narrowPeak exists, skipping"
+fi
+
+# step 7, summit centered bed :
+# Generate non-peaks (background regions) bed file is coming from the macs2 output
+  ## args here : -k9, 9nr sorts by -log10(qvalue) best peaks first, 2-10 = summit position ?
+  ## ± 250 gives 500 window and 4-9 keep name and score.
+if [ ! -f $BASE_SC2/peaks/parietal/parietal_peaks_no_blacklist.bed ]; then
+    echo "$(date) -- creating summit-centered BED"
+    sort -k9,9nr \
+        $BASE_SC2/peaks/parietal/parietal_peaks_blacklisted.narrowPeak \
+        | awk 'BEGIN{OFS="\t"} {
+            start = $2 + $10 - 250
+            end   = $2 + $10 + 250
+            if (start < 0) next
+            print $1, start, end, $4, $9
+        }' \
+        > $BASE_SC2/peaks/parietal/parietal_peaks_no_blacklist.bed
+else
+    echo "$(date) -- BED file exists, skipping"
+fi
 
 
-#todo before chrombpnet main pipeline command (ie finir le pre-processing)
-# 1. define train, validation and test chromosome splits (standard encode cross validation)
-chrombpnet prep splits \
-    -op $BASE_SC2/splits/endoparietal_fold \
-    -c $BASE_SC2/hg38.standard.chrom.sizes \
-    -tcr chr1 \
-    -vcr chr8 chr10
-
-# 2.Generate non-peaks (background regions) bed file is coming from the macs2 output
-# args here : -k9, 9nr sorts by -log10(qvalue) best peaks first, 2-10 = summit position ?
-# ± 250 gives 500 window and 4-9 keep name and score.
-sort -k9,9nr \
-    $BASE_SC2/peaks/parietal/parietal_peaks_filtered.narrowPeak \
-    | awk 'BEGIN{OFS="\t"} {print $1, $2+$10-250, $2+$10+250, $4, $9}' \
-    > $BASE_SC2/peaks/parietal/parietal_peaks_final.bed
+# Step 8 : define train, validation and test chromosome splits (standard encode cross validation)
+if [ ! -f $BASE_SC2/splits/endoparietal_fold.json ]; then
+    echo "$(date) -- creating chromosome splits"
+    chrombpnet prep splits \
+        -op $BASE_SC2/splits/endoparietal_fold \
+        -c $REF/hg38.standard.chrom.sizes \
+        -tcr chr1 \
+        -vcr chr8 chr10
+else
+    echo "$(date) -- splits exist, skipping"
+fi
 
 
-#CHROMBPNET COMMAND , MAIN PIPELINE-------------------------------------------------------------------------
-chrombpnet prep nonpeaks \
-    -g $REF/hg38.fa \
-    -p $BASE_SC2/peaks/parietal/parietal_peaks_no_blacklist.bed \
-    -c $REF/hg38.standard.chrom.sizes \
-    -fl $BASE_SC2/splits/endoparietal_fold.json \
-    -br $REF/blacklist/hg38-blacklist.v2.bed.gz \
-    -o $BASE_SC2/output/parietal/
+#Step 9 and final of preprocessing : generate nonpeak background regions
+if [ ! -f $BASE_SC2/output/parietal/parietal_nonpeaks.bed ]; then
+    echo "$(date) -- generating non-peak regions"
+    chrombpnet prep nonpeaks \
+        -g $REF/hg38.fa \
+        -p $BASE_SC2/peaks/parietal/parietal_peaks_no_blacklist.bed \
+        -c $REF/hg38.standard.chrom.sizes \
+        -fl $BASE_SC2/splits/endoparietal_fold.json \
+        -br $REF/hg38-blacklist.v2.bed.gz \
+        -o $BASE_SC2/output/parietal/
+else
+    echo "$(date) -- non-peaks exist, skipping"
+fi
 
+
+
+#chrombpnet main command
+#apptainer exec --nv $SCRATCH/chrombpnet.sif \
+#    chrombpnet pipeline \
+#        -ibam $BASE_SC2/bam/parietal_shifted_sorted.bam \
+#        -d "ATAC" \
+#        -g $REF/hg38.fa \
+#        -c $REF/hg38.standard.chrom.sizes \
+#        -p $BASE_SC2/peaks/parietal/parietal_peaks_no_blacklist.bed \
+#        -n $BASE_SC2/output/parietal/parietal_nonpeaks.bed \
+#        -fl $BASE_SC2/splits/endoparietal_fold.json \
+#        -b $BASE_SC2/bias_model/ATAC_bias.h5 \
+#        -o $BASE_SC2/chrombpnet_model/parietal/
